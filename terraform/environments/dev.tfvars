38902c3c0b0e104787ce30a847538c4bad50db6f
Kacper Piętka <103852985+KacperPietka@users.noreply.github.com>
2026-05-21 23:12:43 +0200

    feat: Terraform environment-specific variable files

---
+++ terraform/environments/dev.tfvars
@@ -0,0 +1,10 @@
+# Development environment
+# Smaller, cheaper, for testing
+project_id     = "project-192493f2-913e-4971-86c"
+project_number = "626881549611"
+region         = "europe-west1"
+zone           = "europe-west1-b"
+cluster_name   = "kuar-cluster"
+registry_name  = "inference-gateway"
+node_count     = 3
+machine_type   = "e2-medium"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,47 +1,39 @@
 variable "project_id" {
     description = "GCP project ID"
     type = string
-    default = "project-192493f2-913e-4971-86c"
 }
 
 variable "region" {
     description = "GCP region"
     type = string
-    default = "europe-west1"
 }
 
 variable "zone" {
     description = "GCP zone"
     type = string
-    default = "europe-west1-b"
 }
 
 variable "cluster_name" {
     description = "GKE cluster name"
     type = string
-    default = "kuar-cluster"
 }
 
 variable "registry_name" {
     description = "Artifact Registry respository name"
     type = string
-    default = "inference-gateway"
 }
 
 variable "node_count" {
     description = "Number of nodes in the cluster"
     type = number
-    default = 3
 }
 
 variable "machine_type" {
     description = "GKE node machine type"
     type = string
-    default = "e2-medium"
 }
 
 variable "project_number" {
   description = "GCP project number"
   type = string
-  default = "626881549611"
 }
